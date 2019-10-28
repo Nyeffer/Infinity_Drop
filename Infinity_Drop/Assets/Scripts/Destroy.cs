6b23b3a88684f845e916682e61ce26d81061b9d8
Jeffherson Vega <jeffhersonlim@gmail.com>
2019-10-28 13:44:02 -0400

    Testing
    Add more floors to test Destory

--- Infinity_Drop/Assets/Scripts/Destroy.cs
+++ Infinity_Drop/Assets/Scripts/Destroy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Destroy : MonoBehaviour {
 
 	private void OnTriggerEnter(Collider col) { // Whenever a trigger enter this objects range/trigger
-		if(col.tag == "Floor") { // Check if gameObject is a "Floor"
+		if(col.gameObject.tag == "Floor") { // Check if gameObject is a "Floor"
 			Destroy(col.gameObject);  // Destroy Floor
 		}
 	}
