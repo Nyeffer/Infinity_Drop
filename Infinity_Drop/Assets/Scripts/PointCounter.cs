b04baafcb70d2092c3300b743d83360b26bd40ba
Jeffherson Vega <jeffhersonlim@gmail.com>
2019-10-30 16:22:06 -0400

    Point Counting
    Created a point counting script that counts the player Y position multiplied by -1.

---
+++ Infinity_Drop/Assets/Scripts/PointCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCounter : MonoBehaviour {
+
+	// Public Variables
+	public GameObject player; // tracks the Player's Y position multipled by -1
+	public float feetCounted = 0; // access for UI 
+	public Vector3 startPos; // Get the starting position of Player
+
+	// Use this for initialization
+	void Start () {
+		startPos = player.transform.position;
+		feetCounted = (player.transform.position.y - (int)startPos.y) * -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+}
