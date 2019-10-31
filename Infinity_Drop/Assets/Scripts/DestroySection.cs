dd5f93ca9f7db92bd07170e938a864e5f639053a
Jeffherson Vega <jeffhersonlim@gmail.com>
2019-10-31 15:40:35 -0400

    Destroy Section
    Destroy the parent gameObject of the Section when Player is already outside or being too far.

---
+++ Infinity_Drop/Assets/Scripts/DestroySection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySection : MonoBehaviour {
+
+	void OnTriggerExit(Collider col) {
+		if(col.gameObject.tag == "Player") {
+			Destroy(this.gameObject);
+		}
+	}
+}
