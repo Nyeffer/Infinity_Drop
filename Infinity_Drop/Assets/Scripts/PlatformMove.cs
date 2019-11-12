a7ce9e2f98ef5451ff64e2bf469b6ad85b2da628
Jeffherson Vega <jeffhersonlim@gmail.com>
2019-11-12 15:01:47 -0500

    started on moving

--- Infinity_Drop/Assets/Scripts/PlatformMove.cs
+++ Infinity_Drop/Assets/Scripts/PlatformMove.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlatformMove : MonoBehaviour {
 
+	// Public Variables
+	public float distance; // Distance to travel till reset
+	public float timeTillReset; // Time before reset will happen
+
 	// Use this for initialization
 	void Start () {
 		
