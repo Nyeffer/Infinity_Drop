cf22160441bd53c6bce0d5d839f0d9111905ccae
Jeffherson Vega <jeffhersonlim@gmail.com>
2019-11-16 08:31:59 -0500

    Updating

--- Infinity_Drop/Assets/Scripts/PlatformMove.cs
+++ Infinity_Drop/Assets/Scripts/PlatformMove.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class PlatformMove : MonoBehaviour {
 
 	// Public Variables
-	public float distance; // Distance to travel till reset
-	public float timeTillReset; // Time before reset will happen
+	public bool NorS; // A boolean that dictates whether the Platform goes North or South
+	public bool EorW; // A Boolean that dictates whether the Platform goes East or West
+	public float speed; // Multiplier for how fast the platform move
+
+	// Private Variables
+
+	private Vector3 pos; // Vector3 that record the gameObject's position
+
 
 	// Use this for initialization
 	void Start () {
