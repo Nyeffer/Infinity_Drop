7da71f1c7152c18d0213ae0385b105fda62a4958
Jeffherson Vega <jeffhersonlim@gmail.com>
2020-01-31 16:45:04 -0500

    Set up Offset of the Spawns

--- Infinity_Drop/Assets/Scripts/CreateSections.cs
+++ Infinity_Drop/Assets/Scripts/CreateSections.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class CreateSections : MonoBehaviour {
 
 	// Public Variables
-	public GameObject[] Sections; // An array of prefabs/Section that will be chosen randomly
+	public GameObject[] Sections; // An array of prefabs/Section that will be chosen randomly.
 	public int offset = 0; // an integer of the distant between the player and the position of the section.
 
 	private void SpawnSection(int pos) { // Function that will spawn a Section
+		int rand = Random.Range(0, Sections.Length); // Make a number between 0 and the Length of the Array.
 		Vector3 spawnLoc= new Vector3(gameObject.transform.position.x, pos, gameObject.transform.position.z); 
-		Instantiate(Sections[0], spawnLoc, Quaternion.identity);
+		Instantiate(Sections[rand], spawnLoc, Quaternion.identity);
 	}
 
 	void OnTriggerEnter(Collider col) { // Whenever player Enters, Create a new Section.
 		if(col.gameObject.tag == "Player") { // Check object if it's Player
-			int rand = Random.Range(0, Sections.Length);
 			SpawnSection((int)col.gameObject.transform.position.y - offset); // Create the Section
 		}
 	}
